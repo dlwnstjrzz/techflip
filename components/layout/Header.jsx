8cb1d0b7d8ca750586207b65c03fcbb856d2e5fb
noah <qny123@naver.com>
2025-01-14 20:32:51 +0900

    fix: header 변경

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -25,8 +25,10 @@ export default function Header({ showOnlyInSubPages }) {
 
   // 메인 페이지에서는 헤더를 숨김
   if (
-    (showOnlyInSubPages && pathname === "/") ||
-    pathname.includes("compare")
+    showOnlyInSubPages &&
+    pathname === "/"
+    // ||
+    // pathname.includes("compare")
   ) {
     return null;
   }
@@ -40,34 +42,16 @@ export default function Header({ showOnlyInSubPages }) {
       >
         <div className="container mx-auto px-4">
           <div
-            className={cn(
-              "flex items-center justify-between gap-4",
-              isComparePage ? "h-10" : "h-14"
-            )}
+            className={cn("flex items-center justify-between gap-4", "h-14")}
           >
-            {isComparePage ? (
-              <button
-                onClick={() => router.back()}
-                className="flex items-center gap-1 text-muted-foreground hover:text-foreground"
-              >
-                <ArrowLeft className="w-6" />
-              </button>
-            ) : (
-              <Link
-                href="/"
-                className="flex items-center gap-2 font-kanit tracking-tight text-foreground"
-              >
-                MOABA
-                {/* <Image
-                  width={250}
-                  height={40}
-                  src="/images/damoa_logo.jpeg"
-                  alt="Damoa"
-                /> */}
-              </Link>
-            )}
+            <Link
+              href="/"
+              className="flex items-center gap-2 font-kanit tracking-tight text-foreground"
+            >
+              MOABA
+            </Link>
 
-            {!isComparePage && pathname !== "/" && (
+            {pathname !== "/" && (
               <div className="flex-1 max-w-xl">
                 <SearchInput />
               </div>
